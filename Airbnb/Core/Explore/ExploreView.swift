d1c80d6421807113650f3e2ada38f4a55f3a6c28
Sarvar Xudayberdiyev <sxweb20@gmail.com>
2026-06-30 10:13:45 +0500

    Explore View UI

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by sarvar on 30/6/2026.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack (spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
