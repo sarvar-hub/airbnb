33fda057f8dd5d8db28056b6a03c288745303a5d
Sarvar Xudayberdiyev <sxweb20@gmail.com>
2026-06-30 10:29:17 +0500

    Custom Search Bar

--- Airbnb/Core/Explore/ExploreView.swift
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -10,16 +10,20 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack (spacing: 32) {
-                    ForEach(0 ... 10, id: \.self) { listing in
-                        ListingItemView()
-                            .frame(height: 400)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                        
+            VStack {
+                SearchAndFilterBar()
+                
+                ScrollView {
+                    LazyVStack (spacing: 32) {
+                        ForEach(0 ... 10, id: \.self) { listing in
+                            ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                            
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
         }
     }
