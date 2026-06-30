cdae2a81f25dfcb0fb667c9388f9d7e1679c85fb
Sarvar Xudayberdiyev <sxweb20@gmail.com>
2026-06-30 09:15:10 +0500

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by sarvar on 30/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
