29794a877965d0b6a981bb56454cd8cd13ac9bee
Sarvar Xudayberdiyev <sxweb20@gmail.com>
2026-07-01 20:41:29 +0500

    Finish UI

--- Airbnb/Core/Listings/ListingDetailView.swift
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -162,6 +162,7 @@ struct ListingDetailView: View {
             }
             .padding()
         }
+        .toolbar(.hidden, for: .tabBar)
         .ignoresSafeArea()
         .padding(.bottom, 72)
         .overlay(alignment: .bottom){
---
+++ Airbnb/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileOptionRowView.swift
+//  Airbnb
+//
+//  Created by sarvar on 1/7/2026.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Settings")
+}
